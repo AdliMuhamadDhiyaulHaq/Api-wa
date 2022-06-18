df62d67d07bd48464d0b40e4b8fa4b2f24ce1311
AdliMuhamadDhiyaulHaq <77865768+AdliMuhamadDhiyaulHaq@users.noreply.github.com>
2022-06-19 00:16:07 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,59 @@
+const wa = require('@open-wa/wa-automate');
+const http= require('http');
+const express = require('express');
+const app = express();
+const server=http.createServer(app);
+var whatsappi= null;
+app.use(express.urlencoded({
+  extended:true
+}))
+
+app.get('/',(req,res)=>{
+  res.sendFile('public/index.html',{
+    root:__dirname
+  }) 
+});
+
+app.post('/doVerifikasi', async(req,res)=>{
+  var status = 0;
+  var message = null;
+  var phone = req.body.phone;
+  // var pesan = req.body.pesan;
+  if (phone) {
+    var pesan ='Verifikasi kode register '+Math.floor(Math.random() * 10000);
+    await whatsappi.sendText(phone+'@c.us',pesan).then((respon)=>{
+      status=1;
+      message="Berhasil Dikirim";
+    }).catch((err)=>{
+      message="Ada Error"+err;
+    });  
+  }
+  res.status(200).json({
+    status:status,
+    message:message,
+
+  }) 
+});
+wa.create({
+  sessionId: "COVID_HELPER",
+  multiDevice: true, //required to enable multiDevice support
+  authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
+  blockCrashLogs: true,
+  disableSpins: true,
+  headless: true,
+  hostNotificationLang: 'PT_BR',
+  logConsole: false,
+  popup: true,
+  qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+}).then(client => start(client));
+
+function start(client) {
+  whatsappi = client;
+  client.onMessage(async message => {
+    if (message.body === 'Hi') {
+      await client.sendText(message.from, '👋 Hello!');
+    }
+  });
+}
+
+server.listen(8087,()=> console.log('Berhasil Masuk'));
